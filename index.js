31b5c97aa81e69170a7eaa53bf16678e8ecaf26c
Alan2Luna <33102698+Alan2Luna@users.noreply.github.com>
2021-04-23 03:48:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 // Global Variables
-//app.use((req, res, next) => {
+// app.use((req, res, next) => {
    // next();
-})
+// })
 
 // Routes
 app.use(require('./routes/index'))
